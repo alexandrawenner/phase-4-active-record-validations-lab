df947a4c504e79cf7a2fd1354c34fbe86155b099
Alexandra Wenner <alexandrawenner05@gmail.com>
2022-07-26 16:11:32 -0400

    commit tree d0493f25e8c050d558c6910b5d18a4fd0f0b696a

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,16 @@
 class Post < ApplicationRecord
+    validates :title, presence: true
+    validates :content, length: { minimum: 250 }
+    validates :summary, length: { maximum: 250 } 
+    validates :category, inclusion: { in: ["Fiction", "Non-Fiction"] }
+
+    validate :clickbaity_title?
+
+    CLICKBAIT = [/Won't Believe/, /Secret/, /Top [number]/, /Guess/]
+
+    def clickbaity_title?
+       if CLICKBAIT.none? { |word| word.match title }
+        errors.add(:title, "More clickbait!")
+       end
+    end
 end
